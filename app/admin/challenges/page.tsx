7160b963c2de16e464bbd0389ea4146a5468f2ea
rajan <rajannrajb0502@gmail.com>
2025-06-19 20:49:42 +0530

    Fix: add default export to admin challenges page

--- app/admin/challenges/page.tsx
+++ app/admin/challenges/page.tsx
@@ -1,1 +1,8 @@
- 
+export default function AdminChallengesPage() {
+  return (
+    <div>
+      <h1>Admin Challenges</h1>
+      {/* Add admin challenge management UI here */}
+    </div>
+  );
+} 
